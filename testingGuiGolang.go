a3305f282fe7d1488e26dc8e695e112ba403538d
Taryro55 <reply16@protonmail.com>
2022-09-22 22:45:35 -0500

    First GUI working test

---
+++ testingGuiGolang.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"github.com/gen2brain/raylib-go/raylib"
+)
+
+func main() {
+	w, h := int32(800), int32(450)
+	rl.InitWindow(w , h, "Connect 4!")
+	rl.SetTargetFPS(60)
+
+	x, y := int32(0),int32(0)
+
+	for !rl.WindowShouldClose() {
+
+		if (rl.IsKeyPressed(257) && (rl.IsKeyDown(342) || rl.IsKeyDown(346))) {
+			display := rl.GetCurrentMonitor()
+
+			if rl.IsWindowFullscreen() {
+				rl.SetWindowSize(rl.GetMonitorWidth(display), rl.GetMonitorHeight(display))
+			} else {
+				rl.SetWindowSize(int(w), int(h))
+			}
+			rl.ToggleFullscreen()
+		}
+
+		x, y = rl.GetMouseX(), rl.GetMouseY()
+		rl.BeginDrawing()
+		rl.ClearBackground(rl.Black)
+		rl.DrawText("Congrats! You created your first window!", w*24/100, h/2, 20, rl.LightGray)
+		rl.DrawRectangleLines(w*2/100, h*2/100, w-w*4/100, h-h*4/100, rl.LightGray)
+		rl.DrawCircle(x, y, 30, rl.White)
+
+		rl.EndDrawing()
+	}
+
+	rl.CloseWindow()
+}
